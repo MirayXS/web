1eaabe2f5b491437f19cdde03181a3a4784b8077
px1club <55161530+px1club@users.noreply.github.com>
2021-11-17 06:24:13 -0800

    Update index.js

--- index.js
+++ index.js
@@ -5,39 +5,20 @@ const axios = require('axios');
 var bodyParser = require('body-parser')
 const crypto = require('crypto')
 const {machineId, machineIdSync} = require('node-machine-id')
-const Sentry = require("@sentry/node");
-const Tracing = require("@sentry/tracing");
-
-
-
-Sentry.init({
-    dsn: "https://b1c9396e82dd4d6eb24757c5b86a9990@o691975.ingest.sentry.io/5856103",
-    integrations: [
-        // enable HTTP calls tracing
-        new Sentry.Integrations.Http({ tracing: true }),
-      ],
-    
-  
-    // Set tracesSampleRate to 1.0 to capture 100%
-    // of transactions for performance monitoring.
-    // We recommend adjusting this value in production
-    tracesSampleRate: 1.0,
-  });
-
 
 
 
 async function getMachineId() {
     let id = await machineId();
-    
+
 }
 
 
 (async() => {
-let msg = await axios.get(`https://px1-v2api.herokuapp.com/msg`, {
+    let msg = await axios.get(`https://px1-v2api.herokuapp.com/msg`, {
     })
 
-console.log(msg.data.msg)
+    console.log(msg.data.msg)
 })();
 
 var v = "V17"
@@ -125,60 +106,60 @@ app.get(`/start`, async (req, res) => {
             if (resq.data.message !== "Please Refresh the page") {
 
 
-            if (resq.data.used == false) {
+                if (resq.data.used == false) {
 
 
 
 
-                var key = "";
+                    var key = "";
 
 
-                const haship = resq.data.hashedip
-
-
-
-
-
-                var decrypt = crypto.createDecipher('aes-128-cbc', key+id+haship);
-                var s = decrypt.update(resq.data.cookie, 'hex', 'utf8');
-                s += decrypt.final('utf8');
-
-
-
-                var Cookie = s
+                    const haship = resq.data.hashedip
 
 
 
 
-                var CookieIsValid = checkCookie(Cookie)
-                if (CookieIsValid) {
-                    getCookieAuth(Cookie, (Authcode) => {
-                        var Time = Math.floor(+new Date())
-                if(resq.data.vcode == null){
+
+                    var decrypt = crypto.createDecipher('aes-128-cbc', key+id+haship);
+                    var s = decrypt.update(resq.data.cookie, 'hex', 'utf8');
+                    s += decrypt.final('utf8');
 
 
-                res.redirect(`roblox-player:1+launchmode:play+gameinfo:${Authcode}+launchtime:${Time}+placelauncherurl:https%3A%2F%2Fassetgame.roblox.com%2Fgame%2FPlaceLauncher.ashx%3Frequest%3DRequestGame%26browserTrackerId%3D71726228327%26placeId%3D${resq.data.gameid}%26isPlayTogetherGame%3Dfalse+browsertrackerid:71726228327+robloxLocale:en_us+gameLocale:en_us+channel:`)
 
-                }else{
-                    res.redirect(`roblox-player:1+launchmode:play+gameinfo:${Authcode}+launchtime:${Time}+placelauncherurl:https%3A%2F%2Fassetgame.roblox.com%2Fgame%2FPlaceLauncher.ashx%3Frequest%3DRequestPrivateGame%26placeId%3D${resq.data.gameid}%26linkCode%3D${resq.data.vcode}+robloxLocale:en_us+gameLocale:en_us+channel:`)
+                    var Cookie = s
+
+
+
+
+                    var CookieIsValid = checkCookie(Cookie)
+                    if (CookieIsValid) {
+                        getCookieAuth(Cookie, (Authcode) => {
+                            var Time = Math.floor(+new Date())
+                            if(resq.data.vcode == null){
+
+
+                                res.redirect(`roblox-player:1+launchmode:play+gameinfo:${Authcode}+launchtime:${Time}+placelauncherurl:https%3A%2F%2Fassetgame.roblox.com%2Fgame%2FPlaceLauncher.ashx%3Frequest%3DRequestGame%26browserTrackerId%3D71726228327%26placeId%3D${resq.data.gameid}%26isPlayTogetherGame%3Dfalse+browsertrackerid:71726228327+robloxLocale:en_us+gameLocale:en_us+channel:`)
+
+                            }else{
+                                res.redirect(`roblox-player:1+launchmode:play+gameinfo:${Authcode}+launchtime:${Time}+placelauncherurl:https%3A%2F%2Fassetgame.roblox.com%2Fgame%2FPlaceLauncher.ashx%3Frequest%3DRequestPrivateGame%26placeId%3D${resq.data.gameid}%26linkCode%3D${resq.data.vcode}+robloxLocale:en_us+gameLocale:en_us+channel:`)
+                            }
+
+                        })
+                    }
+
+
+
+
+
+
+
+
+                } else {
+                    res.send("Invalid GameKey or used")
                 }
-
-                    })
-                }
-
-
-
-
-
-
-
-
-            } else {
-                res.send("Invalid GameKey or used")
+            }else{
+                res.send('refresh the page')
             }
-        }else{
-            res.send('refresh the page')
-        }
 
         } else {
             res.redirect(`https://github.com/px1club/web/releases/tag/${resq.data.ver}`)
