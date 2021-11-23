e93578d0b6826b0cb6076893f2f6a8afd6444386
px1club <55161530+px1club@users.noreply.github.com>
2021-11-23 15:29:48 -0800

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var bodyParser = require('body-parser')
 const crypto = require('crypto')
 const {machineId, machineIdSync} = require('node-machine-id')
 const readline = require("readline");
+const fs = require('fs')
 
 
 const rl = readline.createInterface({
@@ -13,32 +14,52 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-
-
 async function getMachineId() {
     let id = await machineId();
 
 }
 
 
-(async() => {
+(async () => {
+
+
+    let msg = await axios.get(`https://px1-v2api.herokuapp.com/msg`, {})
+
+    console.log(msg.data.msg)
+
 
     const path = './key.txt'
 
+
     if (!fs.existsSync(path)) {
-        console.log('k')
+        rl.question("ABV3 key (https://linkvertise.com/341222/abv31/1): ", function (key) {
+            const sec = key.replace(/\s+/g, ' ').trim()
+            fs.writeFile('key.txt', sec, function (err) {
+
+            });
+
+        });
     }
-    rl.question("ABV3 key", function(name) {
 
-    });
+    fs.readFile('key.txt', 'utf8', (err, data) => {
+        (async () => {
+            let res = await axios.get(`https://px1-v2api.herokuapp.com/ckey?key=${data}`, {})
+            if (res.data.error == true) {
+                rl.question("ABV3 key (https://linkvertise.com/341222/abv31/1): ", function (key) {
+                    const sec = key.replace(/\s+/g, ' ').trim()
+                    fs.writeFile('key.txt', sec, function (err) {
 
-    let msg = await axios.get(`https://px1-v2api.herokuapp.com/msg`, {
+                    });
+
+                });
+            }
+
+
+        })();
     })
-
-    console.log(msg.data.msg)
 })();
 
-var v = "V17"
+var v = "V18"
 
 
 app.use(bodyParser.json())
@@ -97,103 +118,86 @@ async function checkCookie(input) {
 }
 
 
-
-
 function log(input) {
     if (input) console.log(`[Alt Bot]: ${input.toString()}`)
 }
 
 
-
-
 app.get(`/start`, async (req, res) => {
-    res.header("Content-Type", "text/plain; charset=utf-8");
-    var Key = req.query.GameKey
-    var GameId = req.query.id
-    let id = machineIdSync()
+    fs.readFile('key.txt', 'utf8', (err, data1) => {
+        (async () => {
+            res.header("Content-Type", "text/plain; charset=utf-8");
+            var Key = req.query.GameKey
+            var GameId = req.query.id
+            let id = machineIdSync()
 
 
-    if (Key) {
-
-        let resq = await axios.get(`https://px1-v2api.herokuapp.com/start?GameKey=${Key}&hwid=${id}`, {
-
-        })
-
-        if (resq.data.ver == v) {
-            if (resq.data.message !== "Please Refresh the page") {
+            if (Key) {
 
 
-                if (resq.data.used == false) {
+                let resq = await axios.get(`https://px1-v2api.herokuapp.com/start?GameKey=${Key}&hwid=${id}&key=${data1}`, {})
+
+                if (resq.data.ver == v) {
+                    if (resq.data.message !== "Please Refresh the page") {
 
 
+                        if (resq.data.used == false) {
 
 
-                    var key = "";
+                            var key = "";
 
 
-                    const haship = resq.data.hashedip
+                            const haship = resq.data.hashedip
 
 
+                            var decrypt = crypto.createDecipher('aes-128-cbc', key + id + haship);
+                            var s = decrypt.update(resq.data.cookie, 'hex', 'utf8');
+                            s += decrypt.final('utf8');
 
 
-
-                    var decrypt = crypto.createDecipher('aes-128-cbc', key+id+haship);
-                    var s = decrypt.update(resq.data.cookie, 'hex', 'utf8');
-                    s += decrypt.final('utf8');
+                            var Cookie = s
 
 
-
-                    var Cookie = s
-
-
-
-
-                    var CookieIsValid = checkCookie(Cookie)
-                    if (CookieIsValid) {
-                        getCookieAuth(Cookie, (Authcode) => {
-                            var Time = Math.floor(+new Date())
-                            if(resq.data.vcode == null){
+                            var CookieIsValid = checkCookie(Cookie)
+                            if (CookieIsValid) {
+                                getCookieAuth(Cookie, (Authcode) => {
+                                    var Time = Math.floor(+new Date())
+                                    if (resq.data.vcode == null) {
 
 
-                                res.redirect(`roblox-player:1+launchmode:play+gameinfo:${Authcode}+launchtime:${Time}+placelauncherurl:https%3A%2F%2Fassetgame.roblox.com%2Fgame%2FPlaceLauncher.ashx%3Frequest%3DRequestGame%26browserTrackerId%3D71726228327%26placeId%3D${resq.data.gameid}%26isPlayTogetherGame%3Dfalse+browsertrackerid:71726228327+robloxLocale:en_us+gameLocale:en_us+channel:`)
+                                        res.redirect(`roblox-player:1+launchmode:play+gameinfo:${Authcode}+launchtime:${Time}+placelauncherurl:https%3A%2F%2Fassetgame.roblox.com%2Fgame%2FPlaceLauncher.ashx%3Frequest%3DRequestGame%26browserTrackerId%3D71726228327%26placeId%3D${resq.data.gameid}%26isPlayTogetherGame%3Dfalse+browsertrackerid:71726228327+robloxLocale:en_us+gameLocale:en_us+channel:`)
 
-                            }else{
-                                res.redirect(`roblox-player:1+launchmode:play+gameinfo:${Authcode}+launchtime:${Time}+placelauncherurl:https%3A%2F%2Fassetgame.roblox.com%2Fgame%2FPlaceLauncher.ashx%3Frequest%3DRequestPrivateGame%26placeId%3D${resq.data.gameid}%26linkCode%3D${resq.data.vcode}+robloxLocale:en_us+gameLocale:en_us+channel:`)
+                                    } else {
+                                        res.redirect(`roblox-player:1+launchmode:play+gameinfo:${Authcode}+launchtime:${Time}+placelauncherurl:https%3A%2F%2Fassetgame.roblox.com%2Fgame%2FPlaceLauncher.ashx%3Frequest%3DRequestPrivateGame%26placeId%3D${resq.data.gameid}%26linkCode%3D${resq.data.vcode}+robloxLocale:en_us+gameLocale:en_us+channel:`)
+                                    }
+
+                                })
                             }
 
-                        })
+
+                        } else {
+                            res.send("Invalid GameKey or used")
+                        }
+                    } else {
+                        res.send('refresh the page')
                     }
 
-
-
-
-
-
-
-
                 } else {
-                    res.send("Invalid GameKey or used")
+                    res.redirect(`https://github.com/px1club/web/releases/tag/${resq.data.ver}`)
                 }
-            }else{
-                res.send('refresh the page')
+
+
+            } else {
+                res.send("pls provide an id")
             }
 
-        } else {
-            res.redirect(`https://github.com/px1club/web/releases/tag/${resq.data.ver}`)
-        }
-
-
-    } else {
-        res.send("pls provide an id")
-    }
-
+        })();
+    })
 
 
 })
 
 
-
-
 app.listen(80, () => {
-    log(`App started on port 80.`)
+
 })
